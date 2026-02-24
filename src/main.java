fd6c508d30d8a25484b466a92a7d5c9cb6607086
shreya <sansareshreya6@gmail.com>
2026-02-24 08:50:31 -0500

    restored comments

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import frontend.*;
 
 public class main {
     public static void main(String[] args) {
-												//login page package??
+											
         LoginPage lg = new LoginPage();
     }
 }
