e0e66895691e2fddb823699cfa76700e7699026d
Ware Jay Sunder <warejay003@gmail.com>
2026-03-12 00:25:13 +0530

    Select organizatio page updated

--- src/frontend/SelectOrg.java
+++ src/frontend/SelectOrg.java
@@ -619,12 +619,8 @@ public class SelectOrg extends JFrame{
         profileBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(
-                    null,
-                    "Profile feature is in progress",
-                    "messege",
-                    JOptionPane.INFORMATION_MESSAGE
-                );
+                new ProfilePage(user,role);
+                dispose();
             }
         });
         logoutBtn.addActionListener(new ActionListener() {
